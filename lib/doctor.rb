5be51ddb153e150a974dc23cceadb9beb7316c19
Dmitri Mendis <2dmendis@gmail.com>
2018-06-04 17:47:26 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,7 +12,7 @@ class Doctor
   end 
   
   def new_appointment(date, patient)
-    appOb = Appointment.new(date, patient, self)
+    appOb = Appointment.new(patient, date, self)
   end 
   
   def appointments 
